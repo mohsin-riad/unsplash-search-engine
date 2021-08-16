1441acd4f131a44f7ff9edb8e7ac52b3353b3edb
mohsin <morteen1997@gmail.com>
2021-08-16 18:25:21 +0600

    UI Components and Route Updated

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Index from '../components/Index.vue'
+import Title from '../components/Title.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Index',
+    component: Index,
+    children: [
+      {
+        path: '/',
+        name: 'Title',
+        component: Title,
+      },
+    ]
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
-  }
+  
 ]
 
 const router = createRouter({
